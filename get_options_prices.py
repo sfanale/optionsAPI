90d4a8f4b9b3daf0b36809fd32f7f819852042f8
stephen <stephenfanale@hotmail.com>
2018-10-28 10:04:51 -0400

    -- changed read_one to read_one_all -- Added read_list which returns a list of contracts associated with a ticker -- using ticker.upper in the SQL query in case front end doesnt send all caps

--- get_options_prices.py
+++ get_options_prices.py
@@ -17,7 +17,38 @@ def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
 
-def read_one(ticker):
+def read_list(ticker):
+    """
+    This function responds to a request for /api/options/{ticker}
+    with  a list of known contracts for that symbol
+    :param ticker:   symbol of asset to find
+    :return:        list of known contracts matching symbol
+    """
+    # Does the person exist in people?
+    cur, conn = connect_to_db()
+    resultDict = []
+    try:
+        cur.execute("""SELECT DISTINCT contractsymbol FROM prices WHERE underlyingsymbol = %s ORDER BY contractsymbol;""", (ticker.upper(),))
+        result = cur.fetchall()
+        print(result)
+        print(len(result))
+        print(cur.rowcount)
+        for row in result:
+            resultDict.append({'symbol': ticker, 'contractsymbol':row[0]})
+    # otherwise, nope, not found
+    except ValueError:
+        abort(
+            404, "asset with name {ticker} not found".format(ticker=ticker)
+        )
+    cur.close()
+    conn.close()
+    print(resultDict)
+    print(len(resultDict))
+    return flask.jsonify(resultDict)
+    #print(flask.jsonify(resultDict[0:3]))
+    #return resultDict[0:3]
+
+def read_one_all(ticker):
     """
     This function responds to a request for /api/options/{ticker}
     with  matching assset from options database
@@ -28,7 +59,7 @@ def read_one(ticker):
     cur, conn = connect_to_db()
     resultDict = []
     try:
-        cur.execute("""SELECT pricedate, expiration, strike, lastprice FROM prices WHERE underlyingsymbol = %s;""", (ticker,))
+        cur.execute("""SELECT pricedate, expiration, strike, lastprice FROM prices WHERE underlyingsymbol = %s;""", (ticker.upper(),))
         result = cur.fetchall()
         print(result)
         print(len(result))
@@ -48,4 +79,3 @@ def read_one(ticker):
     return flask.jsonify(resultDict)
     #print(flask.jsonify(resultDict[0:3]))
     #return resultDict[0:3]
-
