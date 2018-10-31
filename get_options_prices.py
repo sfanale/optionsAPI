560f0d25da068cc8331855139e9f09c196da2e01
stephen <stephenfanale@hotmail.com>
2018-10-31 13:11:46 -0400

    -- Added read_one_symbol to swagger and added function -- will return details for that contract

--- get_options_prices.py
+++ get_options_prices.py
@@ -48,6 +48,7 @@ def read_list(ticker):
     #print(flask.jsonify(resultDict[0:3]))
     #return resultDict[0:3]
 
+
 def read_one_all(ticker):
     """
     This function responds to a request for /api/options/{ticker}
@@ -79,3 +80,36 @@ def read_one_all(ticker):
     return flask.jsonify(resultDict)
     #print(flask.jsonify(resultDict[0:3]))
     #return resultDict[0:3]
+
+
+def read_one_symbol(contractsymbol):
+    """
+        This function responds to a request for /api/options/detail/{contractsymbol}
+        with  matching assset from options database
+        :param ticker:   symbol of asset to find
+        :return:        assets matching symbol
+        """
+    # Does the person exist in people?
+    cur, conn = connect_to_db()
+    resultDict = []
+    try:
+        cur.execute("""SELECT pricedate, expiration, strike, lastprice FROM prices WHERE contractsymbol = %s;""",
+                    (contractsymbol.upper(),))
+        result = cur.fetchall()
+        print(result)
+        print(len(result))
+        print(cur.rowcount)
+        for row in result:
+            resultDict.append({'expiry': row[1], 'strike': row[2], 'lastprice': row[3],
+                               'pricedate': row[0], 'timestamp': get_timestamp()})
+    # otherwise, nope, not found
+    except ValueError:
+        abort(
+            404, "asset with name {sym} not found".format(sym=contractsymbol)
+        )
+    cur.close()
+    conn.close()
+    print(resultDict)
+    print(len(resultDict))
+    return flask.jsonify(resultDict)
+
